d54344120cca6944b36c4db6ba5cad7ebf1f9d87
KylePat25481 <kylepatterson1924@gmail.com>
2025-11-28 11:02:54 +0200

    Updated App.jsx to hide header when in showdetail

--- Podcast App/src/App.jsx
+++ Podcast App/src/App.jsx
@@ -1,16 +1,22 @@
-import { Routes, Route } from "react-router-dom";
+import { Routes, Route, useLocation } from "react-router-dom";
 import Header from "./components/UI/Header";
 import Home from "./pages/Home";
 import ShowDetail from "./pages/ShowDetail";
-import FavouritesPage from "./pages/Favourites";
 import { PodcastProvider } from "./context/PodcastContext";
+
 import { PlayerProvider } from "./components/AudioPlayer/PlayerProvider";
 import AudioPlayer from "./components/AudioPlayer/AudioPlayer";
+import FavouritesPage from "./pages/Favourites";
 
 export default function App() {
+  const location = useLocation();
+
+  // Hide header on podcast detail page
+  const hideHeader = location.pathname.startsWith("/show/");
+
   return (
     <>
-      <Header />
+      {!hideHeader && <Header />}
       <PlayerProvider>
         <PodcastProvider>
           <Routes>
@@ -18,6 +24,7 @@ export default function App() {
             <Route path="/show/:id" element={<ShowDetail />} />
             <Route path="/favourites" element={<FavouritesPage />} />
           </Routes>
+
           <AudioPlayer />
         </PodcastProvider>
       </PlayerProvider>
